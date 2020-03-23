6179fb3d7dace9bfb71f4d77e5ce75f6b9646d4e
Abdulkareem Mustapha <50958501+Aphatheology@users.noreply.github.com>
2020-03-23 16:02:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let myName = "Aphatheology";
-let myCourse = ["HTML" , "CSS" , "JavaScript"];
-console.log("My name is " + myName);
-console.log("My courses are " + myCourse);
-
-/* Since the number of courses I am offering is 3 
-i.e odd number, the code below will print all 
-odd numbers between 1 and 200 (inclusive) 
-*/
-for (let i=0; i <= 200; i++) {
-    if (i%2 !== 0) {
-        console.log(i + " ");
-        
-    }
-}
-
+/* Empty file
